da9853ec7c1d3a97172bd138487a326af01dc5f8
Varun Sharma <cto.varun@gmail.com>
2021-07-22 22:02:15 +0530

    loader implemented

--- jquery/scripts/ajax.js
+++ jquery/scripts/ajax.js
@@ -1,10 +1,12 @@
 function getUser() {
-    $("#btn").fadeOut();
+    // $("#btn").fadeOut();
+    $("#loader-container").fadeIn();
     $.get("https://randomuser.me/api/", function (res) {
         const { results } = res;
         const { name: { title, first, last } } = results[0];
         const fullName = $("#username").html() + `<br/> Username :- ${title} ${first} ${last}`;
         $("#username").html(fullName);
-        $("#btn").fadeIn();
+        // $("#btn").fadeIn();
+        $("#loader-container").fadeOut();
     });
 }
