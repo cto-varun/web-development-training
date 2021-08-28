fcf6e65a6b150e55e396fd0bba43ccd3e260f338
Varun Sharma <cto.varun@gmail.com>
2021-08-28 21:26:19 +0530

    optimized the code, added to cart, retrieved from cart

--- ecommerce/assets/js/script.js
+++ ecommerce/assets/js/script.js
@@ -1,97 +1,141 @@
 const products = [{
-        "image": "./assets/imgs/products/top-one.jpeg",
-        "name": "Canary yellow seahorse blazer",
-        "price": "18000",
-        "description": "Here goes the description of the product."
-    },
-    {
-        "image": "./assets/imgs/products/top-two.jpeg",
-        "name": "Starburst kilt trouser",
-        "price": "15000",
-        "description": "Here goes the description of the product."
-    },
-    {
-        "image": "./assets/imgs/products/top-three.jpeg",
-        "name": "The starburst metallic hoodie dress",
-        "price": "28000",
-        "description": "Here goes the description of the product."
-    },
-    {
-        "image": "./assets/imgs/products/top-one.jpeg",
-        "name": "Canary yellow seahorse blazer",
-        "price": "18000",
-        "description": "Here goes the description of the product."
-    },
-    {
-        "image": "./assets/imgs/products/top-two.jpeg",
-        "name": "Starburst kilt trouser",
-        "price": "15000",
-        "description": "Here goes the description of the product."
-    },
-    {
-        "image": "./assets/imgs/products/top-three.jpeg",
-        "name": "The starburst metallic hoodie dress",
-        "price": "28000",
-        "description": "Here goes the description of the product."
-    },
-    {
-        "image": "./assets/imgs/products/top-one.jpeg",
-        "name": "Canary yellow seahorse blazer",
-        "price": "18000",
-        "description": "Here goes the description of the product."
-    },
-    {
-        "image": "./assets/imgs/products/top-two.jpeg",
-        "name": "Starburst kilt trouser",
-        "price": "15000",
-        "description": "Here goes the description of the product."
-    },
-    {
-        "image": "./assets/imgs/products/top-three.jpeg",
-        "name": "The starburst metallic hoodie dress",
-        "price": "28000",
-        "description": "Here goes the description of the product."
-    },
-    {
-        "image": "./assets/imgs/products/top-one.jpeg",
-        "name": "Canary yellow seahorse blazer",
-        "price": "18000",
-        "description": "Here goes the description of the product."
-    },
-    {
-        "image": "./assets/imgs/products/top-two.jpeg",
-        "name": "Starburst kilt trouser",
-        "price": "15000",
-        "description": "Here goes the description of the product."
-    },
-    {
-        "image": "./assets/imgs/products/top-three.jpeg",
-        "name": "The starburst metallic hoodie dress",
-        "price": "28000",
-        "description": "Here goes the description of the product."
-    },
-    {
-        "image": "./assets/imgs/products/top-one.jpeg",
-        "name": "Canary yellow seahorse blazer",
-        "price": "18000",
-        "description": "Here goes the description of the product."
-    },
-    {
-        "image": "./assets/imgs/products/top-two.jpeg",
-        "name": "Starburst kilt trouser",
-        "price": "15000",
-        "description": "Here goes the description of the product."
-    },
-    {
-        "image": "./assets/imgs/products/top-three.jpeg",
-        "name": "The starburst metallic hoodie dress",
-        "price": "28000",
-        "description": "Here goes the description of the product."
-    }
+    "image": "./assets/imgs/products/top-one.jpeg",
+    "name": "Canary yellow seahorse blazer",
+    "price": "18000",
+    "description": "Here goes the description of the product."
+},
+{
+    "image": "./assets/imgs/products/top-two.jpeg",
+    "name": "Starburst kilt trouser",
+    "price": "15000",
+    "description": "Here goes the description of the product."
+},
+{
+    "image": "./assets/imgs/products/top-three.jpeg",
+    "name": "The starburst metallic hoodie dress",
+    "price": "28000",
+    "description": "Here goes the description of the product."
+},
+{
+    "image": "./assets/imgs/products/top-one.jpeg",
+    "name": "Canary yellow seahorse blazer",
+    "price": "18000",
+    "description": "Here goes the description of the product."
+},
+{
+    "image": "./assets/imgs/products/top-two.jpeg",
+    "name": "Starburst kilt trouser",
+    "price": "15000",
+    "description": "Here goes the description of the product."
+},
+{
+    "image": "./assets/imgs/products/top-three.jpeg",
+    "name": "The starburst metallic hoodie dress",
+    "price": "28000",
+    "description": "Here goes the description of the product."
+},
+{
+    "image": "./assets/imgs/products/top-one.jpeg",
+    "name": "Canary yellow seahorse blazer",
+    "price": "18000",
+    "description": "Here goes the description of the product."
+},
+{
+    "image": "./assets/imgs/products/top-two.jpeg",
+    "name": "Starburst kilt trouser",
+    "price": "15000",
+    "description": "Here goes the description of the product."
+},
+{
+    "image": "./assets/imgs/products/top-three.jpeg",
+    "name": "The starburst metallic hoodie dress",
+    "price": "28000",
+    "description": "Here goes the description of the product."
+},
+{
+    "image": "./assets/imgs/products/top-one.jpeg",
+    "name": "Canary yellow seahorse blazer",
+    "price": "18000",
+    "description": "Here goes the description of the product."
+},
+{
+    "image": "./assets/imgs/products/top-two.jpeg",
+    "name": "Starburst kilt trouser",
+    "price": "15000",
+    "description": "Here goes the description of the product."
+},
+{
+    "image": "./assets/imgs/products/top-three.jpeg",
+    "name": "The starburst metallic hoodie dress",
+    "price": "28000",
+    "description": "Here goes the description of the product."
+},
+{
+    "image": "./assets/imgs/products/top-one.jpeg",
+    "name": "Canary yellow seahorse blazer",
+    "price": "18000",
+    "description": "Here goes the description of the product."
+},
+{
+    "image": "./assets/imgs/products/top-two.jpeg",
+    "name": "Starburst kilt trouser",
+    "price": "15000",
+    "description": "Here goes the description of the product."
+},
+{
+    "image": "./assets/imgs/products/top-three.jpeg",
+    "name": "The starburst metallic hoodie dress",
+    "price": "28000",
+    "description": "Here goes the description of the product."
+}
 ];
+const sizeList = ['XS', 'S', 'M', 'L', 'XL'];
+let selectedSize = '';
 
 
+function generateSizeListBoxes(sizeName) {
+    let sizeBoxesContainer = document.getElementById('size-boxes-container');
+    let elem = '';
+    for (const size of sizeList) {
+        elem += `<div class="boxes ${sizeName && sizeName == size ? ' highlight' : ''}" onclick="toggleSizeClass(this)">${size}</div>`;
+    }
+    sizeBoxesContainer.innerHTML = elem;
+}
 
+function toggleSizeClass(box) {
+    selectedSize = box.innerHTML;
+    generateSizeListBoxes(box.innerHTML);
+}
+function addToCart() {
+
+    // 1. Product details like name, description, image, price
+    const { productName, productImage, productPrice, productDescription } = getProductDetailsFromSessionStorage();
+
+    // 2. qunatity selected by user.
+    const quantity = document.getElementById('quantity').value;
+    // 3. selected size by the user.
+    
+    // 4. create an object of all these details and add them to cart array. 
+    if (quantity > 0 && selectedSize != "") {
+        const productDetail = {
+            productName,
+            productImage,
+            productPrice,
+            productDescription,
+            quantity,
+            selectedSize
+        };
+        localStorage.setItem("cart", JSON.stringify(productDetail));
+    } else {
+        alert("Please select size and quantity");
+    }
+}
+
+function getCart() {
+    let cart = localStorage.getItem('cart');
+    cart = JSON.parse(cart);
+    // console.log(cart.productName);
+}
 function generateProducts() {
     let productsContainer = document.getElementById("products-container");
     let elem = '';
@@ -114,20 +158,32 @@ function generateProducts() {
     });
     productsContainer.innerHTML = elem;
 }
-
-function saveToCache(name, price, image, description) {
+function setProductDetailsInSessionStorage(name, price, image, description) {
     localStorage.setItem('productName', name);
     localStorage.setItem('productPrice', price);
     localStorage.setItem('productImage', image);
     localStorage.setItem('productDescription', description);
+}
+function saveToCache(name, price, image, description) {
+    setProductDetailsInSessionStorage(name, price, image, description);
     window.location = "./productDetails.html";
 }
-
-function loadProductDetails() {
+function getProductDetailsFromSessionStorage() {
     const productName = localStorage.getItem("productName");
     const productImage = localStorage.getItem("productImage");
     const productPrice = localStorage.getItem("productPrice");
     const productDescription = localStorage.getItem("productDescription");
+    return {
+        productName,
+        productImage,
+        productPrice,
+        productDescription
+    };
+}
+
+function loadProductDetails() {
+    generateSizeListBoxes();
+    const { productName, productImage, productPrice, productDescription } = getProductDetailsFromSessionStorage();
     if (productName && productPrice && productImage && productDescription) {
         let productNameContainer = document.getElementById("product-name");
         let productPriceContainer = document.getElementById("product-price");
@@ -136,8 +192,8 @@ function loadProductDetails() {
         productNameContainer.innerHTML = productName;
         productPriceContainer.innerHTML = ' ₹ ' + productPrice;
         productDescriptionContainer.innerHTML = productDescription;
-        for (let i = 0; i < productImageContainers.length; i++) {
-            productImageContainers[i].src = productImage;
+        for (const productImageContainer of productImageContainers) {
+            productImageContainer.src = productImage;
         }
     } else {
         window.location = "./products.html";
